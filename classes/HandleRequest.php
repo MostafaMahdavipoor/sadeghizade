a2fdc213960444704ef8e0a6f71ddc00b810dbde
azarakhsh <azarakhshsoleimani@gmail.com>
2025-10-28 14:21:55 +0330

    رفع باگ

--- classes/HandleRequest.php
+++ classes/HandleRequest.php
@@ -8,52 +8,43 @@ trait HandleRequest
 
     public function handleRequest(): void
     {
+        $token = null;
 
         if (str_starts_with($this->text, "/start ")) {
             $token = substr($this->text, 7);
-
-            if ($token === 'register') {
-
-                $this->db->saveUser($this->message["from"], $token);
-
-                $text = "به سامانه ثبت‌نام مشاوره کنکور خوش آمدید.\n\n" .
-                    "برای شروع فرآیند ثبت‌نام، لطفاً روی دکمه زیر کلیک کنید:";
-
-                $buttons = [
-                    'inline_keyboard' => [
-                        [
-                            ['text' => '✅ شروع ثبت نام', 'callback_data' => 'start_registration']
-                        ]
-                    ]
-                ];
-
-                $this->sendRequest("sendMessage", [
-                    "chat_id" => $this->chatId,
-                    "text" => $text,
-                    "reply_markup" => json_encode($buttons)
-                ]);
-
-                return;
-            }
-
-
-            $sectionType = $sectionType ?? null;
-            $this->db->saveUser($this->message["from"], $token);
         }
 
-        $this->db->saveUser($this->message["from"]);
-        $state       = $this->fileHandler->getState($this->chatId);
-        $isAdmin     = $this->db->isAdmin($this->chatId);
+        $this->db->saveUser($this->message["from"], $token);
+        $isAdmin = $this->db->isAdmin($this->chatId);
+        $state = $this->fileHandler->getState($this->chatId);
+
+        if ($token === 'register') {
+
+            $text = "به سامانه ثبت‌نام مشاوره کنکور خوش آمدید.\n\n" .
+                    "برای شروع فرآیند ثبت‌نام، لطفاً روی دکمه زیر کلیک کنید:";
+
+            $buttons = [
+                'inline_keyboard' => [
+                    [
+                        ['text' => '✅ شروع ثبت نام', 'callback_data' => 'start_registration']
+                    ]
+                ]
+            ];
+
+            $this->sendRequest("sendMessage", [
+                "chat_id" => $this->chatId,
+                "text" => $text,
+                "reply_markup" => json_encode($buttons)
+            ]);
+            return;
+        }
 
         if ($this->text == "/start") {
             $this->fileHandler->saveState($this->chatId, null);
-            $this->db->saveUser($this->message["from"]);
-
-
-            $this->showMainMenu();
-
-
+            $this->showMainMenu($isAdmin);
             return;
         }
+
+       
     }
 }
