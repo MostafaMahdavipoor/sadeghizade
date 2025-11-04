a3e427f990b1b5e1dc593b799210eedcfbd332d9
azarakhsh <azarakhshsoleimani@gmail.com>
2025-11-04 11:27:16 +0330

    fix bug

--- classes/BotHandler.php
+++ classes/BotHandler.php
@@ -314,7 +314,6 @@ class BotHandler
 
         if ($callbackData === 'add_next_subject') {
 
-            $this->saveCurrentEntryToDb($data);
             $data['current_entry'] = [];
             $this->fileHandler->saveData($this->chatId, $data);
             $this->fileHandler->saveState($this->chatId, 'awaiting_report_input');
